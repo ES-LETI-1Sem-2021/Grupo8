c2fe0356315decbc7f7890dbe329bcdea9b4ee81
fmslz-iscte <fmslz@iscte-iul.pt>
2021-12-07 01:51:43 +0000

    JavaDoc

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ESGRUPO8.esgrupo8"}];updateSearchResults();
---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ESGRUPO8.esgrupo8","l":"GitHubApi"},{"p":"ESGRUPO8.esgrupo8","l":"TrelloApi"}];updateSearchResults();
